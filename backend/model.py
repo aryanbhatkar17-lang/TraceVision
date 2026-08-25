e5ce70e60c3f6265029ff52299dca70910d82afb
Aryan Bhatkar <aryanbhatkar17@gmail.com>
2026-08-25 21:29:08 +0530

    replace deprecated F.tanh with torch.tanh, add type hints

--- backend/model.py
+++ backend/model.py
@@ -1,56 +1,55 @@
+from typing import Tuple
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import math
-#import pytorch_colors as colors
-import numpy as np
+
 
 class EnhanceNetNoPool(nn.Module):
+    """Zero-DCE enhancement network without pooling layers.
 
-	def __init__(self):
-		super(EnhanceNetNoPool, self).__init__()
+    Predicts pixel-wise illumination adjustment curves from a low-light
+    input, producing an enhanced image in a single forward pass.
 
-		self.relu = nn.ReLU(inplace=True)
+    Input:  (B, 3, H, W) tensor in [0, 1]
+    Output: tuple of (enhance_image_1, enhance_image, r) where:
+      - enhance_image_1: intermediate enhanced image
+      - enhance_image: final enhanced image
+      - r: predicted curve parameter maps (B, 24, H, W)
+    """
 
-		number_f = 32
-		self.e_conv1 = nn.Conv2d(3,number_f,3,1,1,bias=True) 
-		self.e_conv2 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
-		self.e_conv3 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
-		self.e_conv4 = nn.Conv2d(number_f,number_f,3,1,1,bias=True) 
-		self.e_conv5 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
-		self.e_conv6 = nn.Conv2d(number_f*2,number_f,3,1,1,bias=True) 
-		self.e_conv7 = nn.Conv2d(number_f*2,24,3,1,1,bias=True) 
+    def __init__(self) -> None:
+        super().__init__()
+        self.relu = nn.ReLU(inplace=True)
+        nf = 32  # number of filters per conv layer
+        self.e_conv1 = nn.Conv2d(3, nf, 3, 1, 1, bias=True)
+        self.e_conv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.e_conv3 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.e_conv4 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.e_conv5 = nn.Conv2d(nf * 2, nf, 3, 1, 1, bias=True)
+        self.e_conv6 = nn.Conv2d(nf * 2, nf, 3, 1, 1, bias=True)
+        self.e_conv7 = nn.Conv2d(nf * 2, 24, 3, 1, 1, bias=True)
 
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        x1 = self.relu(self.e_conv1(x))
+        x2 = self.relu(self.e_conv2(x1))
+        x3 = self.relu(self.e_conv3(x2))
+        x4 = self.relu(self.e_conv4(x3))
+        x5 = self.relu(self.e_conv5(torch.cat([x3, x4], 1)))
+        x6 = self.relu(self.e_conv6(torch.cat([x2, x5], 1)))
 
-		
-	def forward(self, x):
+        x_r = torch.tanh(self.e_conv7(torch.cat([x1, x6], 1)))
+        r1, r2, r3, r4, r5, r6, r7, r8 = torch.split(x_r, 3, dim=1)
 
-		x1 = self.relu(self.e_conv1(x))
-		# p1 = self.maxpool(x1)
-		x2 = self.relu(self.e_conv2(x1))
-		# p2 = self.maxpool(x2)
-		x3 = self.relu(self.e_conv3(x2))
-		# p3 = self.maxpool(x3)
-		x4 = self.relu(self.e_conv4(x3))
-
-		x5 = self.relu(self.e_conv5(torch.cat([x3,x4],1)))
-		# x5 = self.upsample(x5)
-		x6 = self.relu(self.e_conv6(torch.cat([x2,x5],1)))
-
-		x_r = F.tanh(self.e_conv7(torch.cat([x1,x6],1)))
-		r1,r2,r3,r4,r5,r6,r7,r8 = torch.split(x_r, 3, dim=1)
-
-
-		x = x + r1*(torch.pow(x,2)-x)
-		x = x + r2*(torch.pow(x,2)-x)
-		x = x + r3*(torch.pow(x,2)-x)
-		enhance_image_1 = x + r4*(torch.pow(x,2)-x)		
-		x = enhance_image_1 + r5*(torch.pow(enhance_image_1,2)-enhance_image_1)		
-		x = x + r6*(torch.pow(x,2)-x)	
-		x = x + r7*(torch.pow(x,2)-x)
-		enhance_image = x + r8*(torch.pow(x,2)-x)
-		r = torch.cat([r1,r2,r3,r4,r5,r6,r7,r8],1)
-		return enhance_image_1,enhance_image,r
+        x = x + r1 * (torch.pow(x, 2) - x)
+        x = x + r2 * (torch.pow(x, 2) - x)
+        x = x + r3 * (torch.pow(x, 2) - x)
+        enhance_image_1 = x + r4 * (torch.pow(x, 2) - x)
+        x = enhance_image_1 + r5 * (torch.pow(enhance_image_1, 2) - enhance_image_1)
+        x = x + r6 * (torch.pow(x, 2) - x)
+        x = x + r7 * (torch.pow(x, 2) - x)
+        enhance_image = x + r8 * (torch.pow(x, 2) - x)
+        r = torch.cat([r1, r2, r3, r4, r5, r6, r7, r8], 1)
+        return enhance_image_1, enhance_image, r
 
 
 
